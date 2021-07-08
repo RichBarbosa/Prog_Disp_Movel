996eb283971b19b3f8416e8ab31f11cabba75df5
RichBarbosa <70115573+RichBarbosa@users.noreply.github.com>
2021-07-08 03:00:32 -0300

    app 12
    App 12

---
+++ ExemploFragment/app/src/main/java/com/richard/exemplofragment/PagerAdapter.kt
@@ -0,0 +1,30 @@
+package com.richard.exemplofragment
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class PagerAdapter(gerenciador: FragmentManager): FragmentPagerAdapter(gerenciador) {
+    override fun getCount(): Int {
+        return 3
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return if (position == 0)
+            Fragment01()
+        else if(position == 1)
+            Fragment02()
+        else
+            Fragment03()
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return if (position == 0)
+            "Conversa"
+        else if (position == 1)
+            "Status"
+        else
+            "Teste"
+    }
+
+}
