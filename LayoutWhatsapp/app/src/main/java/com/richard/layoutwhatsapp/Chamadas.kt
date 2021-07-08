1935b57c8e1d45956a50187984cf204fb823bcaa
RichBarbosa <70115573+RichBarbosa@users.noreply.github.com>
2021-07-08 16:29:32 -0300

    app 13
    App 13

---
+++ LayoutWhatsapp/app/src/main/java/com/richard/layoutwhatsapp/Chamadas.kt
@@ -0,0 +1,25 @@
+package com.richard.layoutwhatsapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class Chamadas : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_conversas, container, false)
+    }
+
+
+}
